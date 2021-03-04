a6f1b460274f38d5d362314d206779e0884016a6
alu0100785630 <alu0100785630@ull.edu.es>
2021-03-04 12:30:42 +0000

    Dependencias morgan y nodemon + Primeros Middlewares

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+
+//Morgan permite ver el estado de las peticiones en la terminal
+const morgan = require('morgan');
+
+
 const app = express();
 
+//Con el argumento especificamos como queremos que sea el loggin
+app.use(morgan('dev'));
+
+//Esto es un Middleware, es una función que puede modificar los datos entrantes de una petición
+//La petición pasa a través de esto mientras se procesa
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.status(200).send('Test para comprobar que el servidor funciona');
+  res.status(200).send('Test para comprobar que el servidor');
 });
 
 
